864b89d01ee654d7a868490e580579a8ce35c568
IvanGML <[somovlife@gmail.com]>
2018-02-27 17:04:19 +0300

    From Android. Logout works now.

--- app/router/router.js
+++ app/router/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StackNavigator, TabNavigator, DrawerNavigator } from 'react-navigation';
+import { Button, Image, StyleSheet } from "react-native";
 
 import { LogoPage, SignUpScreen } from '../features/LogoPage';
 import { PlaygroundPage } from '../features/PlaygroundPage';
@@ -8,6 +9,40 @@ import { ActionBarPage } from '../features/ActionBarPage';
 import { ProfilePage } from '../features/ProfilePage';
 import { EditProfilePage } from '../features/EditProfilePage/';
 import * as style from '../styles';
+import { connect } from 'react-redux';
+import { userLogout } from '../features/LogoPage/actions';
+import { firebaseApp } from '../firebase';
+
+
+class DumpLogout extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Logout',
+  };
+  componentDidMount()  {
+    this.props.userLogout();
+    firebaseApp.auth().signOut();
+    this.props.navigation.navigate('Login page')
+  }
+
+  render() {
+    return (
+      <Button
+        onPress={()=>{}}
+        title="Logout"
+      />
+    );
+  }
+}
+
+const Logout = connect(null, { userLogout })(DumpLogout);
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
+
 
 const Drawer = DrawerNavigator({
  Playground: {
@@ -18,6 +53,9 @@ const Drawer = DrawerNavigator({
  },
  About: {
    screen: AboutPage,
+ },
+ Logout: {
+   screen: Logout,
  }
 })
 
